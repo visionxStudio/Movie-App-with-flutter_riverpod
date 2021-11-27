d708ea46133e3760b1ea90bd8012e7e53bc81848
officialvisionx <manish.rajak2055@gmail.com>
2021-11-27 08:55:19 +0545

    adding image assets constants

---
+++ lib/src/common/constants/assets.dart
@@ -0,0 +1,7 @@
+class ImageAsset {
+  ImageAsset._();
+  static const String _base = 'assets/images';
+
+  // For splash screen
+  static const String kLogo = "$_base/logo.png";
+}
---
+++ lib/src/features/splash/splash_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  final VoidCallback onInitializationComplete;
+  const SplashPage({Key? key, required this.onInitializationComplete})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
