bbd18e6526d50d247aa2d1541fe21842231d55cb
officialvisionx <manish.rajak2055@gmail.com>
2021-11-27 16:56:04 +0545

    home page widgets designing

--- lib/src/features/home/home_page.dart
+++ lib/src/features/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,10 +8,53 @@ class Homepage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Center(
-        child: Text("data"),
+    final double _height = MediaQuery.of(context).size.height;
+    final double _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        height: _height,
+        width: _width,
+        child: Stack(
+          children: [
+            _backgroundWidget(height: _height, width: _width),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _backgroundWidget({double? height, double? width}) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            "https://loyolamaroon.com/wp-content/uploads/2021/03/Godzilla-v-Kong.jpg",
+          ),
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 15.0,
+          sigmaY: 15.0,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _foregroundWidgets({required double height, required double width}) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, height * 0.02, 0, 0),
+      width: width * 0.88,
     );
   }
 }
